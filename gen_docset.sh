a61564843124564b5f3b32d31341b3626ca52b0b
Lucas Wilson-Richter <lucas.wilsonrichter@redbubble.com>
2021-05-28 12:19:21 +1000

    OK, not that slow

--- gen_docset.sh
+++ gen_docset.sh
@@ -2,8 +2,8 @@ mkdir -p aws-cdk-ts.docset/Contents/Resources/Documents/api
 cp resources/Info.plist aws-cdk-ts.docset/Contents/Info.plist
 cp resources/docSet.dsidx aws-cdk-ts.docset/Contents/Resources/docSet.dsidx
 
-wget -nH -np -P tmp --recursive --wait 1 --random-wait https://docs.aws.amazon.com/cdk/api/latest/typescript/index.html
-wget -nH -np -P tmp --recursive --wait 1 --random-wait https://docs.aws.amazon.com/cdk/api/latest/typescript/api/toc.html
+wget -nH -np -P tmp --recursive --wait 0.2 --random-wait https://docs.aws.amazon.com/cdk/api/latest/typescript/index.html
+wget -nH -np -P tmp --recursive --wait 0.2 --random-wait https://docs.aws.amazon.com/cdk/api/latest/typescript/api/toc.html
 wget https://docs.aws.amazon.com/cdk/api/latest/docs/aws-construct-library.html -O tmp/aws-construct-library.html
 
 cp -r tmp/cdk/api/latest/typescript/{fonts,styles} aws-cdk-ts.docset/Contents/Resources/Documents
